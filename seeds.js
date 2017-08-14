e4edb6ee8e6e824e9e4c763ce036530e0ece7157
Grave Rider <donprayer@gmail.com>
2017-08-15 00:47:32 +0300

    Added styling to comments page

--- seeds.js
+++ seeds.js
@@ -6,17 +6,17 @@ var data = [
     {
         name: "Cloud's Rest",
         image: "https://farm9.staticflickr.com/8422/7842069486_c61e4c6025.jpg",
-        description: "blah blah blah"
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dictum risus urna, nec ornare urna lacinia et. Suspendisse consectetur ultricies fermentum. Phasellus laoreet lacus nibh, ut venenatis leo ornare in. Curabitur dignissim lorem lacus, sed feugiat velit vulputate auctor. Pellentesque aliquam nisi in lacus interdum, at convallis mi dictum. Morbi ac ex vestibulum, viverra lorem in, feugiat est. Aliquam sapien nulla, vehicula ac rutrum et, auctor non lacus. Vestibulum sed augue id tellus finibus vehicula. Fusce eget augue maximus, gravida elit et, bibendum est. Praesent quis molestie tellus. Proin id dictum tellus. Nunc accumsan tempor dapibus. "
     },
     {
         name: "Raven's Hills",
         image: "https://farm8.staticflickr.com/7259/7121858075_7375241459.jpg",
-        description: "blah blah blah"
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dictum risus urna, nec ornare urna lacinia et. Suspendisse consectetur ultricies fermentum. Phasellus laoreet lacus nibh, ut venenatis leo ornare in. Curabitur dignissim lorem lacus, sed feugiat velit vulputate auctor. Pellentesque aliquam nisi in lacus interdum, at convallis mi dictum. Morbi ac ex vestibulum, viverra lorem in, feugiat est. Aliquam sapien nulla, vehicula ac rutrum et, auctor non lacus. Vestibulum sed augue id tellus finibus vehicula. Fusce eget augue maximus, gravida elit et, bibendum est. Praesent quis molestie tellus. Proin id dictum tellus. Nunc accumsan tempor dapibus. "
     },
     {
         name: "Desert Mesa",
         image: "https://farm3.staticflickr.com/2116/2164766085_0229ac3f08.jpg",
-        description: "blah blah blah"
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dictum risus urna, nec ornare urna lacinia et. Suspendisse consectetur ultricies fermentum. Phasellus laoreet lacus nibh, ut venenatis leo ornare in. Curabitur dignissim lorem lacus, sed feugiat velit vulputate auctor. Pellentesque aliquam nisi in lacus interdum, at convallis mi dictum. Morbi ac ex vestibulum, viverra lorem in, feugiat est. Aliquam sapien nulla, vehicula ac rutrum et, auctor non lacus. Vestibulum sed augue id tellus finibus vehicula. Fusce eget augue maximus, gravida elit et, bibendum est. Praesent quis molestie tellus. Proin id dictum tellus. Nunc accumsan tempor dapibus. "
     }
 ];
 
